a309ad77f8470ce522d90c427d5c366339838d6b
Augusto Cesar <dev.augusto.crp@gmail.com>
2022-12-26 23:09:58 -0300

    Update mapreduce.py

--- mapreduce.py
+++ mapreduce.py
@@ -9,12 +9,11 @@ def pick_animal_type(animal):
     return animal["type"], 1
 
 def reducer(acc, val):
-    print(val)
     if val[0] not in acc.keys():
         acc[val[0]] = 0 + val[1]
     else:
         acc[val[0]] = 0 + val[1] + val[1]
-    print(acc)
+        
 
 type_animals = list(map(pick_animal_type, animals))
 print(type_animals)
